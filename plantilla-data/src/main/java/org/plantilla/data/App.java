bb976b5bb3fd87a8017fbcda1408f769fa21124d
developjavs <123106650+developjavs@users.noreply.github.com>
2023-07-19 21:00:39 -0600

    Version inicial plantilla Devops

---
+++ plantilla-data/src/main/java/org/plantilla/data/App.java
@@ -0,0 +1,13 @@
+package org.plantilla.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
